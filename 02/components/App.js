e3b623c6249005fedf73bc56cd4ad1530fd9583c
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-23 23:34:21 +0100

    Task 02

--- 02/components/App.js
+++ 02/components/App.js
@@ -1,6 +1,7 @@
 // ./src/components/App.js
 import React from 'react';
 import Box from './Box';
+import AppContext from './context';
 
 class App extends React.Component {
     state = {
@@ -8,9 +9,14 @@ class App extends React.Component {
     };
 
     render() {
+        const { Provider: AppContextProvider } = AppContext;
         const { text } = this.state;
 
-        return <Box text={text} />;
+        return (
+            <AppContextProvider value={text}>
+                <Box />
+            </AppContextProvider>
+        );
     }
 }
 
